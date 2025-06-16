475ffb91e9f58952bfe29ed2327cd48ea9fb02c4
felixblablabla <felixdrews333@gmail.com>
2025-06-16 11:06:48 +0200

    enemies sterben

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -11,3 +11,12 @@ func _ready():
 func _physics_process(delta):
 	velocity=Vector2(speed,0).rotated(dir)
 	move_and_slide()
+
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider and collider.is_in_group("enemies"):
+			if collider.has_method("on_hit"):
+				collider.on_hit()
+			queue_free()  # Projektil verschwindet nach dem Treffer
+			break
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
 @export var speed: float = 150.0  # Geschwindigkeit 
-
-
-
+var treffer: int = 0 # Leben mäßig
 
 func _process(delta):
 	position.x -= speed * delta
 
 	if position.x < -800:  
+		queue_free()
+
+func on_hit():
+	treffer += 1
+	if treffer >=2:
 		queue_free()
